4994fcb7a97a76def094ab3d386ff67236ebecd0
Hector Leon Zarco Garcia <hezarco@gmail.com>
2016-02-21 22:38:36 +0100

    remove arrows

--- test/index.js
+++ test/index.js
@@ -2,15 +2,15 @@ var expect = require('chai').expect;
 var assert = require('assert');
 var psaux = require('../lib/psaux');
 
-describe('psaux', () => {
-  describe('#List', () => {
-    it('Return a promise', () => {
+describe('psaux', function() {
+  describe('#List', function() {
+    it('Return a promise', function() {
       expect(psaux).to.be.a('function');
       expect(psaux()).to.be.an.instanceof(Promise);
     });
   });
 
-  describe('#Filter', () => {
+  describe('#Filter', function() {
     it('Apply filters on the returned process list', () => {
       expect(psaux).to.be.a('function');
     });
